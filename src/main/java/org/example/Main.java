b9fc6990deadcad2fcbaa32627cfab2879f8fb23
unknown <ruchellguilherme100@gmail.com>
2023-01-04 19:51:57 -0300

    instanciando a classe carro e setando suas devidas informaçoes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Carro carro = new Carro();
+
+        carro.setAno(2000);
+        carro.setCor("azul");
+        carro.setMarca("fiat");
+        carro.setModelo("uno");
 
     }
 }
