83caacf8c9fc95949b81a916cf7841db87f8b0a2
pKafski <kafski.patrick@gmail.com>
2023-01-04 19:35:45 -0300

    adicionada estrutura do carro

---
+++ src/main/java/org/example/Carro.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Carro {
+    String modelo;
+    String cor;
+    String marca;
+    int ano;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
